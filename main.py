c4c770c14a1812f7509deb519f3535b0d0614110
Anton <antony2bs2lol@gmail.com>
2021-05-14 20:42:56 +0300

    v 2.1

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import requests
 from bs4 import BeautifulSoup
+import csv
 
 get_answer = input('Choose what do you want to know? ')
 
@@ -8,6 +9,7 @@ URL = 'https://www.hltv.org/'
 DICTIONARY = {'user-agent': 'Mozilla/5.0 (Windows NT 10.0; '
                             'Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.141 '
                             'Safari/537.36 OPR/73.0.3856.438', 'accept': '*/*'}
+FILE = 'teams.csv'
 
 
 def get_html(url, params=None):
@@ -25,17 +27,29 @@ def get_content(html):
             teams.append({
                 'name': item.find('a', class_='text-ellipsis').get_text(strip=True),
                 'position': item.find('a', class_='rankNum').get_text(strip=True).replace('.', ''),
-                'logo': item.find('img', class_='teamImg').get('src')
             })
-        print(teams)
+        return teams
     else:
-        print("Sorry, but this program doesn't know how to complete your request :(")
+        return 1
+
+
+def save_file(items, path):
+    with open(path, 'w', newline='') as file:
+        writer = csv.writer(file, delimiter=';')
+        if get_answer == 'Rating':
+            writer.writerow(['Position', 'Team'])
+            for item in items:
+                writer.writerow([item['position'], item['name']])
 
 
 def parse():
     html = get_html(URL)
     if html.status_code == 200:
-        get_content(html.text)
+        teams = get_content(html.text)
+        if teams != 1:
+            save_file(teams, FILE)
+        else:
+            print("Sorry, but this program doesn't know how to complete your request :(")
     else:
         print('Error')
 
